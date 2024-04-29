51191a324bc0f45d773a0168ce943013e8021b1d
Ali Reza Farahnak <alfarahn@microsoft.com>
2024-04-29 18:58:51 +0300

    using default blazor template

--- AirChat.Frontend/Program.cs
+++ AirChat.Frontend/Program.cs
@@ -1,12 +1,13 @@
-
 using AirChat.API.Services;
+using AirChat.Frontend;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
 var client = new AirChatServiceClient(new HttpClient { BaseAddress = new Uri("https://localhost:7155") });
 builder.Services.AddSingleton(client);
 
