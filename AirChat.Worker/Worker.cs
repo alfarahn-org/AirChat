a665482320acef44cacb0298b5f343e9680fabe5
dhari mohammed <b.shehri1234@gmail.com>
2024-04-30 12:52:33 +0300

    air chat worker

---
+++ AirChat.Worker/Worker.cs
@@ -0,0 +1,24 @@
+namespace AirChat.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    await Console.Out.WriteLineAsync("test worker");
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
